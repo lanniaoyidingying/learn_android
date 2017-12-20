256731faf467e6baceb6d5ac1f621709d6be1e75
chuang <lanniaoyidingying@outlook.com>
2017-12-21 00:34:35 +0800

    Desgraption:commit layout Date:2017.12.21 Author:chuang

---
+++ app/src/main/java/com/example/administrator/gittest/activity/layout/LayoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.gittest.activity.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.gittest.R;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framlayout);
+    }
+}
